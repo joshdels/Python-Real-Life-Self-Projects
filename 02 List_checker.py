d94039feeeccadfe2c68a73f61da80881bef7136
joshdels <deleonjoshy2@gmail.com>
2025-05-02 15:48:02 +0800

    made some changes

---
+++ 02 List_checker.py
@@ -0,0 +1,11 @@
+# this is my list checker cause im lazy hehehe
+# this will give me result if its in the list or not by inputing the number
+# got this idea in work since i need to find the field id with the list 
+
+n = int(input("num: "))
+
+list = [75313, 84206, 84207, 85814, 85815, 85816, 87738, 87739, 87740, 87741, 87742, 87743, 87744, 87745, 87746, 87747, 87748, 87749, 155099, 155100, 155101, 192214]
+if n in list:
+    print(f"Yess naa {n}")
+else:
+    print("wala man boss!")
